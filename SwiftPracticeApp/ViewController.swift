3016e09c6cfccf8ebb52aafb7fa17e8e38709d69
okatomo <kaguratomato1@gmail.com>
2022-02-18 16:59:33 +0900

    Initial Commit

---
+++ SwiftPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPracticeApp
+//
+//  Created by Tomoki on 2022/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
